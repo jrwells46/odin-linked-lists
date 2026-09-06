7263fa0e8bcc62d45ddfd366047f1ed81d0210dd
Jason Wells <44629534+jrwells46@users.noreply.github.com>
2026-09-06 12:16:52 -0700

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hello');
+import LinkedList from './linkedlist.js';
+
+const list = new LinkedList();
+
+list.append('dog');
+list.append('cat');
+list.append('parrot');
+list.append('hamster');
+list.append('snake');
+list.append('turtle');
+
+console.log(list.toString());
